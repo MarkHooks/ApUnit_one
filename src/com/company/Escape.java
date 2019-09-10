563e2a2b91f3a7dabec7f1a26adafbecb1bd8526
MarkHooks <hooksma174@gmail.com>
2019-09-10 14:55:44 -0400

    finished part one

---
+++ src/com/company/Escape.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Escape {
+    public static void main(String[] args) {
+        System.out.print("What is the difference between");
+        System.out.print("a ' and a \"?");
+        System.out.print(" Or between a \" and a \\\"?\n");
+        System.out.print("One is what we see when we're typing our program.\nThe other is what appears on the \"console.\"\n");
+        System.out.println("  \\/");
+        System.out.println(" \\\\//");
+        System.out.print("\\\\\\///\n///\\\\\\\n //\\\\\n  /\\\n");
+       }
+}
--- src/com/company/Printing.java
+++ src/com/company/Printing.java
@@ -2,6 +2,11 @@ package com.company;
 
 public class Printing {
     public static void main(String[] args) {
-
+        System.out.println("02:34");
+        System.out.println("09/10/2019");
+        System.out.println("it is a great \ntime to be a \ncomputer programmer");
+        System.out.print("It is a great");
+        System.out.print(" time to be a");
+        System.out.print(" computer programmer");
     }
 }
