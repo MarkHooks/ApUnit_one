97b5265dfd5da894829b516c91f0c45e5032c160
MarkHooks <hooksma174@gmail.com>
2019-09-12 08:38:38 -0400

    fixed the code

--- src/com/company/Meta.java
+++ src/com/company/Meta.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class Meta {
         public static void main(String[] args) {
-            System.out.println("public class Hello {\n    public static void main(String[] args) {\n    System.out.println(\"Hello, world!\");\n  }\n}\n");
+            System.out.println("public class Hello {\n    public static void main(String[] args) {\n      System.out.println(\"Hello, world!\");\n  }\n}\n");
        }
 }
 
