8fdfdd9f937255f20247f5feec3ffad6495d93de
MarkHooks <hooksma174@gmail.com>
2019-09-13 10:14:13 -0400

    finished the painting room + extra

--- src/com/company/Painting.java
+++ src/com/company/Painting.java
@@ -1,16 +1,21 @@
 package com.company;
-
+import java.util.Scanner;
 public class Painting {
     public static void main(String[] args) {
-        int length = 32;
-        int width = 40;
-        int height = 16;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("What is the length of the room?");
+        double length = scan.nextDouble();
+        System.out.print("What is the width of the room?");
+        double width = scan.nextDouble();
+        System.out.print("What is the height of the room?");
+        double height = scan.nextDouble();
+        final int paint = 350;
         int door = 40;
         int window = 60;
-        int celling = length *width;
-        int roomarea = ((width * height) * 2) + ((length * height) * 2);
-        int paintroom = (roomarea - door - window) + celling;
-        double paintneed = paintroom/ 350;
+        double celling = length *width;
+        double roomarea = ((width * height) * 2) + ((length * height) * 2);
+        double paintroom = (roomarea - door - window) + celling;
+        double paintneed = Math.round(paintroom/ 350);
         System.out.print("you will need "+ paintneed+" buckets of paint to paint a room with the area of " +paintroom);
     }
 }
