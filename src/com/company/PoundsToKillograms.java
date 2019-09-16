0b2c484ba72835ed4a532d7012e0eb0ba8c02798
MarkHooks <hooksma174@gmail.com>
2019-09-16 10:45:43 -0400

    converted lbs to killograms

---
+++ src/com/company/PoundsToKillograms.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class PoundsToKillograms {
+    public static void main(String[] args) {
+        double pounds = 115.0;
+        double killogramconv = pounds/2.205;
+        System.out.print(killogramconv);
+    }
+}
