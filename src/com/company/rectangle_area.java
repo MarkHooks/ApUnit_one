d25c413107233ff86aa68e78d55e82ba0eef7261
MarkHooks <hooksma174@gmail.com>
2019-09-16 10:42:40 -0400

    printed area

---
+++ src/com/company/rectangle_area.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class rectangle_area {
+    public static void main(String[] args) {
+        int length = 14;
+        int width = 6;
+        int area = width * length;
+        System.out.print("Area =" + area);
+    }
+}
